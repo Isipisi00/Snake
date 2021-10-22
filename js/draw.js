ec64bc5aaad97a61bf2218bcb8cc2c9295c9f04c
Isipisi00 <gonzalezaparicio.2000@gmail.com>
2021-10-22 13:40:32 +0200

    canvas properies

--- js/draw.js
+++ js/draw.js
@@ -4,3 +4,11 @@ const scale = 10;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 
+let snake = [  {x: 200, y: 200},  {x: 190, y: 200},  {x: 180, y: 200},  {x: 170, y: 200},  {x: 160, y: 200},];
+
+main();
+    
+// main function called repeatedly to keep the game running
+function main() {
+
+}
